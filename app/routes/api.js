6dbc7f203925534b50cf132d7ba8dbda86e8c6de
philippschulte <philipp.schulte@ymail.com>
2016-07-22 21:03:40 -0600

    Add user routes

--- app/routes/api.js
+++ app/routes/api.js
@@ -3,12 +3,18 @@
 // Dependencies
 const express = require('express');
 
+// Controller
+const userCtrl = require('../controllers/userCtrl');
+
 // Router
 const router = express.Router();
 
-//Routes
-router.get('/test', (req, res, next) => {
-  res.json({message: 'This is just a test!'});
-});
+// Routes
+
+// Users
+router.post('/users', userCtrl.create);
+router.get('/users/:userId', userCtrl.read);
+router.put('/users/:userId', userCtrl.update);
+router.delete('/users/:userId', userCtrl.delete);
 
 module.exports = router;
